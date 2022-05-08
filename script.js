2c6420639c365a147d9adb069d6f87aefcf7abaa
RafaelaTieri <rafaelaiwamoto302@gmail.com>
2022-05-08 13:45:51 -0300

    Mudanças finais

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const botao = document.getElementsByClassName("mudar");
+const botao1 = document.querySelector('.mudar')
 
 function mudar_modo() {
     var elem = document.body
     elem.classList.toggle("dark_mode");
+
+    if (elem.classList == 'dark_mode') {
+
+        botao1.innerHTML = 'Light';
+
+    } else {
+        botao1.innerHTML = 'Dark';
+    }
 }
